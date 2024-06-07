a12c620271e0b2f5d95ea09298c226b52eb8650e
expandsource <expandsource@gmail.com>
2024-06-07 12:07:52 +0900

    스프링부트 회원가입 요청

---
+++ ch13_login/src/main/java/com/busanit/ch13_login/activity/TitleActivity.kt
@@ -0,0 +1,26 @@
+package com.busanit.ch13_login.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.busanit.ch13_login.databinding.ActivityTitleBinding
+
+class TitleActivity : AppCompatActivity() {
+    lateinit var binding: ActivityTitleBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTitleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 로그인 버튼을 클릭하면 LoginActivity 로 이동
+        binding.buttonLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        // 회원가입 버튼을 클릭하면 RegisterActivity 로 이동
+        binding.buttonRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+    }
+}
